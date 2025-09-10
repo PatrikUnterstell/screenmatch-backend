beb3a4a5218fe861e73112966ef929e10e73fdb3
PatrikUnterstell <tiquee@gmail.com>
2025-09-10 11:31:53 -0300

    ajuste no CorsConfiguration

--- src/main/java/br/com/alura/screenmatch/config/CorsConfiguration.java
+++ src/main/java/br/com/alura/screenmatch/config/CorsConfiguration.java
@@ -1,6 +1,7 @@
 package br.com.alura.screenmatch.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.lang.NonNull;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -8,9 +9,9 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class CorsConfiguration implements WebMvcConfigurer {
 
     @Override
-    public void addCorsMappings(CorsRegistry registry) {
+    public void addCorsMappings(@NonNull CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://127.0.0.1:5500, http://localhost:5500")
+                .allowedOrigins("http://127.0.0.1:5500", "http://localhost:5500")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT");
     }
 }
